3a3aa760f7744a4a5dad6d40620e4f4611942536
Matteo Cavallaro <teoc98@users.noreply.github.com>
2026-03-08 16:46:27 +0100

    support deployment on subpath

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ function noSearchDefaultPageRender() {
           <input 
             type="text" 
             class="url-input"
-            value="${window.location.origin}/?q=%s"
+            value="${window.location.href.split('?')[0]}?q=%s"
             readonly 
           />
           <button class="copy-button">
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || "/",
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
