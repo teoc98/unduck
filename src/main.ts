6e2519c9fd3cef3546ee3909cbccf17aa52b0c8c
Matteo Cavallaro <teoc98@users.noreply.github.com>
2026-03-08 16:23:34 +0100

    change footer

--- src/main.ts
+++ src/main.ts
@@ -21,11 +21,7 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
-        •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        <a href="https://github.com/teoc98/unduck" target="_blank">github</a>
       </footer>
     </div>
   `;
