be2c47f1f9534634e94bdda2424e0fc909f8d550
Matteo Cavallaro <teoc98@users.noreply.github.com>
2026-03-08 16:53:42 +0100

    fix: use loadEnv to read VITE_BASE_URL properly

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-  base: process.env.VITE_BASE_URL || "/",
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_BASE_URL || "/",
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+      }),
+    ],
+  };
 });
